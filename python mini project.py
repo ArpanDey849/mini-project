823a100f535003edf50b65f6d7770dc115a9b34c
ArpanDey <deyarpan430@gmail.com>
2025-01-26 12:15:21 +0530

    Add files via upload

---
+++ python mini project.py
@@ -0,0 +1,18 @@
+#Guess the number
+import random #(its a library which can generate random charecter)
+target_num=random.randint(1,100)#randint function gives a random number which is btw the given range of 1-100
+while True:#loop end na hoa obdi number same thakbe as kaj loop er modhey cholche so jotokhon na randint function abar call hochee totokhon number will same loop end hole abar prothome cursor giye randit call hbe and then number change before that  number will be same.
+
+    guess_num=input("write your guessed number or Quit(type Q):")
+    if (guess_num=="Q"):
+        break
+    guess_num=int(guess_num)
+    if(target_num==guess_num):
+        print("sucess")
+        break#as soon as break execute loop will break
+    elif(target_num>guess_num):
+        print("guess a big number")
+    else:
+        print("guess a small number")
+    
+print("Game over") 
