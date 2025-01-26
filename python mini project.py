1965871d9d409675d6b1dc6a30077676ffe2cf88
ArpanDey <deyarpan430@gmail.com>
2025-01-26 14:22:57 +0530

    Update python mini project.py

--- python mini project.py
+++ python mini project.py
@@ -1,31 +1,26 @@
-# # Guess the number
-# import random #(its a library which can generate random charecter)
-# target_num=random.randint(1,100)#randint function gives a random number which is btw the given range of 1-100
-# while True:
-
-    # guess_num=input("write your guessed number or Quit(type Q):")
-    # if (guess_num=="Q"):
-    #     break
-    # guess_num=int(guess_num)
-    # if(target_num==guess_num):
-    #     print("sucess")
-    #     break#as soon as break execute loop will break
-    # elif(target_num>guess_num):
-    #     print("guess a big number")
-    # else:
-    #     print("guess a small number")
-    
-# print("Game over") 
+# Guess the number
+import random #(its a library which can generate random charecter)
+target_num=random.randint(1,100)#randint function gives a random number which is btw the given range of 1-100
+while True:
+    guess_num=input("write your guessed number or Quit(type Q):")
+    if (guess_num=="Q"):
+        break
+    guess_num=int(guess_num)
+    if(target_num==guess_num):
+        print("sucess")
+        break#as soon as break execute loop will break
+    elif(target_num>guess_num):
+        print("guess a big number")
+    else:
+        print("guess a small number")
+print("Game over") 
 
 #Random Password Generator
 import random
-import string #this have different type charecter inside it.
+import string #this library has different type charecter inside it.
 letters=string.ascii_letters#this variable have all a-z and A-Z letters and we get the value in the form of string.
-# print(letters)
 digits=string.digits#it give all 0-9 digits
-# print(digits)
 puncutaion=string.punctuation #it gives all special charecter
-# print(puncutaion)
 charvalues=puncutaion+letters+digits
 value=random.choice(charvalues)
 password_len=12
